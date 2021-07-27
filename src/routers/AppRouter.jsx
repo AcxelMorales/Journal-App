11e6cacd36a29efa01905c3ec345e0d93ddc086b
Acxel Morales <acxelmorales97@gmail.com>
2021-07-27 15:10:33 -0500

    Add routes and child routes

---
+++ src/routers/AppRouter.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
+
+import { AuthRouter } from './AuthRouter';
+import { JournalScreen } from '../components/journal/JournalScreen';
+
+export const AppRouter = () => {
+  return (
+    <>
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/auth" component={AuthRouter} />
+            <Route path="/" exact component={JournalScreen} />
+            <Redirect to="/auth/login" />
+          </Switch>
+        </div>
+      </Router>
+    </>
+  );
+};
