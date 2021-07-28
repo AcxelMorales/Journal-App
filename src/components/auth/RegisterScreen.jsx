4b0deac6d8934860184b11be70800c89ecea300f
Acxel Morales <acxelmorales97@gmail.com>
2021-07-27 20:38:32 -0500

    Add styles and structure of view Login - Register

--- src/components/auth/RegisterScreen.jsx
+++ src/components/auth/RegisterScreen.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const RegisterScreen = () => {
   return (
-    <div>
-      <h1>RegisterScreen</h1>
-    </div>
+    <>
+      <h3 className="auth__title">Register</h3>
+      <form>
+        <input
+          type="text"
+          autoComplete="off"
+          placeholder="John Doe"
+          name="name"
+          className="auth__input"
+        />
+        <input
+          type="text"
+          autoComplete="off"
+          placeholder="email@email.com"
+          name="email"
+          className="auth__input"
+        />
+        <input
+          type="password"
+          autoComplete="off"
+          placeholder="******"
+          name="password"
+          className="auth__input"
+        />
+        <input
+          type="password"
+          autoComplete="off"
+          placeholder="******"
+          name="password-confirm"
+          className="auth__input"
+        />
+        <button className="btn btn-primary btn-block mb-5" type="submit">
+          Register
+        </button>
+        <Link to="/auth/login">¿Already registered?</Link>
+      </form>
+    </>
   );
 };
