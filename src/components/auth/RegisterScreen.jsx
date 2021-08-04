b391ea1f6d4dddd60298947f6b1397a283689c70
Acxel Morales <acxelmorales97@gmail.com>
2021-08-04 00:17:18 -0500

    Added library and validators

--- src/components/auth/RegisterScreen.jsx
+++ src/components/auth/RegisterScreen.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import validator from 'validator';
 
 import { useForm } from '../../hooks/useForm';
 
 export const RegisterScreen = () => {
   //TODO: Eliminar data hardcode
-  const [{ name, email, password, passwordConfirm }, handleInputChange, reset] = useForm({
+  const [{ name, email, password, passwordConfirm }, handleInputChange] = useForm({
     name: 'Acxel',
     email: 'acxel@email.com',
     password: 123456,
@@ -14,17 +15,34 @@ export const RegisterScreen = () => {
 
   const handleOnSubmit = evt => {
     evt.preventDefault();
-    reset();
+
+    if (isFormValid) {
+      console.log(true);
+    }
   };
 
   const isFormValid = () => {
+    if (name.trim().length === 0) {
+      console.log('Name is required');
+      return false;
+    } else if (!validator.isEmail(email)) {
+      console.log('Email is not valid');
+      return false;
+    } else if (password !== passwordConfirm || password.length < 5) {
+      console.log('Password should be at leaste 6 characters and match each other');
+      return false;
+    }
 
+    return true;
   };
 
   return (
     <>
       <h3 className="auth__title">Register</h3>
       <form onSubmit={handleOnSubmit}>
+        <div className="auth__error-alert">
+          Hola mundo
+        </div>
         <input
           type="text"
           autoComplete="off"
