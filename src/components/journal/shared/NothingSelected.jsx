589b4f0ec3a0fa662c01c3674b2ad617b9fd1fd7
Acxel Morales <acxelmorales97@gmail.com>
2021-08-05 22:45:37 -0500

    Deleted warning

--- src/components/journal/shared/NothingSelected.jsx
+++ src/components/journal/shared/NothingSelected.jsx
@@ -5,7 +5,7 @@ import image from './svgs/no-selected.svg';
 export const NothingSelected = () => {
   return (
     <div className="nothing__main-content">
-      <img className="nothing__img-waiting" src={image} alt="image-svg" />
+      <img className="nothing__img-waiting" src={image} alt="svg" />
       <p>Select something or create an entry</p>
     </div>
   );
