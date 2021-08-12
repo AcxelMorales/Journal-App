eef936946509827f47d5efc719aae3204896d47e
Acxel Morales <acxelmorales97@gmail.com>
2021-08-12 01:33:03 -0500

    Deleting note

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -107,3 +107,21 @@ export const startUploadingAct = file => {
     dispatch(startSaveNote(activeNote))
   };
 };
+
+export const startDeleting = id => {
+  return async (dispatch, getState) => {
+    const { uid } = getState().auth;
+
+    try {
+      await db.doc(`${uid}/journal/notes/${id}`).delete();
+      dispatch(deleteNote(id));
+    } catch (error) {
+      console.log('Error in deleting', error);
+    }
+  };
+};
+
+export const deleteNote = id => ({
+  type: types.notesDeleted,
+  payload: id,
+});
--- src/components/notes/NoteScreen.jsx
+++ src/components/notes/NoteScreen.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { activeNote } from '../../actions/notes';
+import { activeNote, startDeleting } from '../../actions/notes';
 
 import { useForm } from '../../hooks/useForm';
 
@@ -23,6 +23,8 @@ export const NoteScreen = () => {
   useEffect(() => {
     dispatch(activeNote(formValues.id, { ...formValues }));
   }, [formValues, dispatch]);
+
+  const handleOnDelete = () => dispatch(startDeleting(formValues.id));
 
   return (
     <div className="notes__main-content animate__animated animate__fadeIn">
@@ -51,6 +53,9 @@ export const NoteScreen = () => {
           </div>
         )}
       </div>
+      <button onClick={handleOnDelete} className="btn btn-danger-p">
+        Delete
+      </button>
     </div>
   );
 };
