ff77513e7ee22a12ba3c43c9759d4a04d48a4224
Acxel Morales <acxelmorales97@gmail.com>
2021-09-15 02:51:50 -0500

    Setup testing and added first test

---
+++ src/tests/types/types.test.js
@@ -0,0 +1,25 @@
+import { types } from '../../types/types';
+
+describe('Pruebas con nuestros tipos', () => {
+  test('Debe de tener estos types', () => {
+    expect(types).toEqual({
+      login: '[AUTH] Login',
+      logout: '[AUTH] Logout',
+
+      uiSetError: '[UI] Set Error',
+      uiRemoveError: '[UI] Remove Error',
+      uiStartLoading: '[UI] Start loading',
+      uiFinishLoading: '[UI] Finish loading',
+      uiStartUploading: '[UI] Start uploading',
+      uiFinishUploading: '[UI] Finish uploading',
+
+      notesAddNew: '[NOTES] New note',
+      notesActive: '[NOTES] Set active note',
+      notesLoad: '[NOTES] Load notes',
+      notesUpdated: '[NOTES] Updated note',
+      notesFileUrl: '[NOTES] Updated image url',
+      notesDeleted: '[NOTES] Deleted note',
+      notesLogoutCleaning: '[NOTES] Logout cleaning',
+    });
+  });
+});
